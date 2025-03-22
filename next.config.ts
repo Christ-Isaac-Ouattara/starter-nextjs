c96d7b71a3ff109aedb091e90997dfe9df3415b4
Christ-Isaac-Ouattara <ouattaraisaacpro@gmail.com>
2025-03-22 02:01:11 +0000

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true
+  },
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     turbo: {
       resolveExtensions: [
